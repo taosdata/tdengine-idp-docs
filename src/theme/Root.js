22dddca679d9cc032e77819283bb6fcf7611db5e
chenhaoran <haoran920c@163.com>
2025-07-24 16:58:37 +0800

    fix: refactor navbar hiding logic to use constants for class names and improve event listener management

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,35 +1,42 @@
 import React, { useEffect } from 'react';
 
+const TOC_LINK_SELECTOR = '.table-of-contents__link';
+const NAVBAR_HIDDEN_CLASS = 'navbarHidden_bnBw';
+
 export default function Root({ children }) {
   useEffect(() => {
     const navbar = document.querySelector('.theme-layout-navbar');
     const body = document.body;
+    const tocContainer = document.querySelector('.table-of-contents');
+
     const hideNavbar = () => {
-      navbar?.classList.add('navbarHidden_bnBw');
+      navbar?.classList.add(NAVBAR_HIDDEN_CLASS);
     };
-    // Hide the top navbar when medium-zoom is opened
 
     const observer = new MutationObserver(() => {
       if (body.classList.contains('medium-zoom--opened')) {
         hideNavbar();
       } else {
-        navbar?.classList.remove('navbarHidden_bnBw');
+        navbar?.classList.remove(NAVBAR_HIDDEN_CLASS);
       }
     });
     observer.observe(body, { attributes: true, attributeFilter: ['class'] });
 
-    // Hide the navbar when clicking on a link in the table of contents
     const handleTocClick = (e) => {
-      if (e.target.matches('.table-of-contents__link')) {
+      if (e.target.matches(TOC_LINK_SELECTOR)) {
         hideNavbar();
       }
     };
-    document.addEventListener('click', handleTocClick);
 
-    // cleanup function to remove the event listeners
+    if (tocContainer) {
+      tocContainer.addEventListener('click', handleTocClick);
+    }
+
     return () => {
       observer.disconnect();
-      document.removeEventListener('click', handleTocClick);
+      if (tocContainer) {
+        tocContainer.removeEventListener('click', handleTocClick);
+      }
     };
   }, []);
 
